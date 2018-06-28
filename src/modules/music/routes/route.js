b07536b258f9adbb1e9b29dfb2f0f8cae721d20d
mob35 <toonelicious@gmail.com>
2018-06-28 10:16:43 +0700

    commit

--- src/modules/music/routes/route.js
+++ src/modules/music/routes/route.js
@@ -14,11 +14,38 @@ module.exports = function (app) {
     //     .get(controller.read)
     //     .put(controller.update)
     //     .delete(controller.delete);
-    app.route('/api/music')
+    app.route('/api/music1')
         .get(controller.getList)
         .post(controller.create);
 
-    app.route('/api/music/:id')
+    app.route('/api/music1/:id')
+        .get(controller.read)
+        .put(controller.update)
+        .delete(controller.delete);
+
+    app.route('/api/music2')
+        .get(controller.getList)
+        .post(controller.create);
+
+    app.route('/api/music2/:id')
+        .get(controller.read)
+        .put(controller.update)
+        .delete(controller.delete);
+
+    app.route('/api/music3')
+        .get(controller.getList)
+        .post(controller.create);
+
+    app.route('/api/music3/:id')
+        .get(controller.read)
+        .put(controller.update)
+        .delete(controller.delete);
+
+    app.route('/api/music4')
+        .get(controller.getList)
+        .post(controller.create);
+
+    app.route('/api/music4/:id')
         .get(controller.read)
         .put(controller.update)
         .delete(controller.delete);
